2a4ecff6b0bfd9660ce7af350be7a184ee09d2e0
Jesse <jesse.heitler@torqueim.com>
2014-06-01 03:42:22 +0100

    Mock up: example call list.

--- volunteering/urls.py
+++ volunteering/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     url(r'^import/$', 'volunteering.views.importer', name='import'),
     url(r'^claim/$', TemplateView.as_view(template_name='volunteering/claim.html')),
+    url(r'^call_list/$', TemplateView.as_view(template_name='volunteering/call_list.html')),
 )
