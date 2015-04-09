f7ce596b6fbee2676a91aaedf0659a8e6e78c0eb
Jesse <jesse.heitler@torqueim.com>
2015-04-09 13:24:45 +0100

    Create robots.txt.

--- dothis/urls.py
+++ dothis/urls.py
@@ -1,15 +1,17 @@
-from django.views.generic import TemplateView
-from django.contrib import admin
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-#from djrill import DjrillAdminSite
-
-#print("JESSE setting djrill as the admin site")
-#admin.site = DjrillAdminSite()
-
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='dothis/home.html'), name='home'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', TemplateView.as_view(
+        template_name='dothis/home.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^volunteering/', include('volunteering.urls', namespace='volunteering')),
+    url(r'^volunteering/', include('volunteering.urls',
+                                   namespace='volunteering')),
     url(r'^styles/', include('foundation.urls')),
+    url(r'^robots\.txt$', lambda r: HttpResponse(
+        content="User-agent: *\nDisallow: /*",
+        content_type="text/plain")),
 )
