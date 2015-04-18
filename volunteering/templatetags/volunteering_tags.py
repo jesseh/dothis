05ee079499e963b1a530d316165bb180824603c3
Jesse <jesse.heitler@torqueim.com>
2015-04-18 02:38:28 +0100

    Fix: event report template file name.

--- volunteering/templatetags/volunteering_tags.py
+++ volunteering/templatetags/volunteering_tags.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('results.html')
+@register.inclusion_tag('volunteering/event_report.html')
 def event_report(assignment):
     return {'event': assignment.duty.event}
