33ccc3c8a5958eb62467dead9c18cf1d099ce507
Jesse <jesse.heitler@torqueim.com>
2015-04-22 02:27:28 +0100

    Use template tags for complex data.

--- volunteering/templatetags/volunteering_tags.py
+++ volunteering/templatetags/volunteering_tags.py
@@ -1,10 +1,27 @@
 from datetime import datetime
 from django import template
 
+from volunteering.models import Assignment
+
 register = template.Library()
 
 
-@register.inclusion_tag('volunteering/event_report.html')
-def event_report(assignment):
-    return {'event': assignment.duty.event,
+@register.inclusion_tag('volunteering/tags/event_report.html')
+def event_report(assignment_or_event):
+    if isinstance(assignment_or_event, Assignment):
+        event = assignment_or_event.duty.event
+    else:
+        event = assignment_or_event
+
+    return {'event': event,
             'generated_datetime': datetime.now(), }
+
+
+@register.inclusion_tag('volunteering/tags/volunteered_for.html')
+def volunteered_for(volunteer):
+    return {'volunteer': volunteer}
+
+
+@register.inclusion_tag('volunteering/tags/assignment_detail.html')
+def assignment_detail(assignment):
+    return {'assignment': assignment}
--- volunteering/tests/test_tags.py
+++ volunteering/tests/test_tags.py
@@ -7,14 +7,21 @@ import factories as f
 
 
 class TestEventReport(TestCase):
-    def test_RendersTheEventName(self):
+    def testEventReport_RendersTheEventName_GivenAnAssignment(self):
         assignment = f.AssignmentFactory()
         template = Template(
             "{% load volunteering_tags %} {% event_report assignment %}")
         rendered = template.render(Context({'assignment': assignment}))
         self.assertIn('Event report', rendered)
 
-    def test_RendersAllTheVolunteerNamesRelatedToThatAssignmentAndDuty(self):
+    def testEventReport_RendersTheEventName_GivenAnEvent(self):
+        event = f.EventFactory()
+        template = Template(
+            "{% load volunteering_tags %} {% event_report event %}")
+        rendered = template.render(Context({'event': event}))
+        self.assertIn('Event report', rendered)
+
+    def testEventReport_RendersAllTheVolunteerNamesRelatedToThatAssignmentAndDuty(self):
         v1, v2 = f.VolunteerFactory.create_batch(2)
         event = f.EventFactory()
         duty = f.DutyFactory(event=event)
@@ -26,7 +33,7 @@ class TestEventReport(TestCase):
         self.assertIn(v1.first_name, rendered)
         self.assertIn(v2.first_name, rendered)
 
-    def test_RendersMultipleDutiesAssociatedWithOneEvent(self):
+    def testEventReport_RendersMultipleDutiesAssociatedWithOneEvent(self):
         v1, v2 = f.VolunteerFactory.create_batch(2)
         event = f.EventFactory()
         duty1, duty2 = f.DutyFactory.create_batch(2, event=event)
@@ -37,3 +44,33 @@ class TestEventReport(TestCase):
         rendered = template.render(Context({'assignment': assignment}))
         self.assertIn(v1.first_name, rendered)
         self.assertIn(v2.first_name, rendered)
+
+
+class TestVolunteeredFor(TestCase):
+    def testVolunteeredFor_ListsAllFutureAssignmentsForAVolunteer(self):
+        event1 = f.EventFactory(name="e1name")
+        event2 = f.EventFactory(name="e2name")
+        event3 = f.EventFactory(name="e3name")
+        volunteer = f.VolunteerFactory()
+        duty1 = f.DutyFactory(event=event1)
+        duty2 = f.DutyFactory(event=event2)
+        f.AssignmentFactory(volunteer=volunteer, duty=duty1)
+        f.AssignmentFactory(volunteer=volunteer, duty=duty2)
+        template = Template(
+            "{% load volunteering_tags %} {% volunteered_for volunteer %}")
+        rendered = template.render(Context({'volunteer': volunteer}))
+        self.assertIn(event1.name, rendered)
+        self.assertIn(event2.name, rendered)
+        self.assertNotIn(event3.name, rendered)
+
+
+class TestAssignmentDetail(TestCase):
+    def testAssignmentDetail_SaysTheDetails(self):
+        event = f.EventFactory(name="yada")
+        volunteer = f.VolunteerFactory()
+        duty = f.DutyFactory(event=event)
+        assignment = f.AssignmentFactory(volunteer=volunteer, duty=duty)
+        template = Template(
+            "{% load volunteering_tags %} {% assignment_detail assignment %}")
+        rendered = template.render(Context({'assignment': assignment}))
+        self.assertIn(event.name, rendered)
