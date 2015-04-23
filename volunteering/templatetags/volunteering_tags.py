619ca4483f610551ea1ea69e097d2dc3cc362774
Jesse <jesse.heitler@torqueim.com>
2015-04-23 13:41:33 +0100

    Help text for template tags

--- volunteering/templatetags/volunteering_tags.py
+++ volunteering/templatetags/volunteering_tags.py
@@ -6,20 +6,24 @@ register = template.Library()
 
 @register.inclusion_tag('volunteering/tags/event_report.html')
 def event_report(event):
+    """Full detail of duties and volunteers assigned to an event."""
     return {'event': event,
             'generated_datetime': datetime.now(), }
 
 
 @register.inclusion_tag('volunteering/tags/volunteered_for.html')
 def volunteered_for(volunteer):
+    """List of assignments for which a volunteer has volunteered."""
     return {'volunteer': volunteer}
 
 
 @register.inclusion_tag('volunteering/tags/assignment_detail.html')
 def assignment_detail(assignment):
+    """Details of an assignment including what, where and when."""
     return {'assignment': assignment}
 
 
 @register.inclusion_tag('volunteering/tags/duty_volunteers.html')
 def duty_volunteers(duty):
+    """List of the names of volunteers who are assigned to a duty."""
     return {'duty': duty}
